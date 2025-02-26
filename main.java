541c5069c08b8a360099b653769439673cfa8bed
Edward <97175962+Jegijs@users.noreply.github.com>
2025-02-26 12:09:38 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,20 @@
 
+public class Main {
+    public static void main(String[] args) {
+
+
+        Person person1 = UserInput.getPersonFromUser();
+
+        System.out.println("------------");
+        System.out.println("Person details:");
+        System.out.println(person1);
+
+        //todo - saprast un izveidot pareizu izvadi console ievaditajiem datiem
+
+
+
+
+
+
+    }
+}
